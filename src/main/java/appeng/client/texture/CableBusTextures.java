07bfdc5d85fca1b8dac5f73e8607defa6448e829
Caedis <Caedis@users.noreply.github.com>
2024-04-30 22:51:17 -0500

    cache missing texture

--- src/main/java/appeng/client/texture/CableBusTextures.java
+++ src/main/java/appeng/client/texture/CableBusTextures.java
@@ -11,6 +11,7 @@
 package appeng.client.texture;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
@@ -214,6 +215,7 @@ public enum CableBusTextures {
 
     private final String name;
     public IIcon IIcon;
+    private static TextureAtlasSprite missingTexture = null;
 
     CableBusTextures(final String name) {
         this.name = name;
@@ -223,10 +225,15 @@ public enum CableBusTextures {
         return null;
     }
 
+
     @SideOnly(Side.CLIENT)
     public static IIcon getMissing() {
-        return ((TextureMap) Minecraft.getMinecraft().getTextureManager().getTexture(TextureMap.locationBlocksTexture))
-                .getAtlasSprite("missingno");
+        if (missingTexture == null) {
+            missingTexture = ((TextureMap) Minecraft.getMinecraft().getTextureManager().getTexture(TextureMap.locationBlocksTexture))
+                    .getAtlasSprite("missingno");
+        }
+
+        return missingTexture;
     }
 
     public String getName() {
