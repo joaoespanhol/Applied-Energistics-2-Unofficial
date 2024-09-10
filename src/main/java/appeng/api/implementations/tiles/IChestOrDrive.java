412928feb55d7c2cfebdfeca573fb6f6fa0e651d
MCTBL <wszth0130@outlook.com>
2024-09-10 23:54:19 +0800

    add default return for new method

--- src/main/java/appeng/api/implementations/tiles/IChestOrDrive.java
+++ src/main/java/appeng/api/implementations/tiles/IChestOrDrive.java
@@ -38,7 +38,9 @@ public interface IChestOrDrive extends ICellContainer, IGridHost, IOrientable {
      * @param slot slot index
      * @return status of the slot, one of the above indices.
      */
-    int getCellStatus(int slot);
+    default int getCellStatus(int slot) {
+        return 0;
+    }
 
     /**
      * 0 - item cell,
