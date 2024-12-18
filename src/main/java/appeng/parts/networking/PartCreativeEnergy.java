34c653a0f2617372aca25ee0329ee5ed832270d1
lordIcocain <lordicocain@gmail.com>
2024-12-18 12:59:57 +0200

    no_extra_connection

--- src/main/java/appeng/parts/networking/PartCreativeEnergy.java
+++ src/main/java/appeng/parts/networking/PartCreativeEnergy.java
@@ -1,46 +1,26 @@
 package appeng.parts.networking;
 
-import java.util.EnumSet;
-
 import net.minecraft.client.renderer.RenderBlocks;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
-import net.minecraftforge.common.util.ForgeDirection;
 
 import org.lwjgl.opengl.GL11;
 
 import appeng.api.config.AccessRestriction;
 import appeng.api.config.Actionable;
 import appeng.api.config.PowerMultiplier;
-import appeng.api.networking.GridFlags;
-import appeng.api.networking.IGridNode;
 import appeng.api.networking.energy.IAEPowerStorage;
 import appeng.api.parts.IPartCollisionHelper;
-import appeng.api.parts.IPartHost;
 import appeng.api.parts.IPartRenderHelper;
 import appeng.client.texture.TextureUtils;
-import appeng.me.helpers.AENetworkProxy;
 import appeng.parts.AEBasePart;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class PartCreativeEnergy extends AEBasePart implements IAEPowerStorage {
 
-    private final AENetworkProxy outerProxy = new AENetworkProxy(
-            this,
-            "outer",
-            this.getProxy().getMachineRepresentation(),
-            true);
-
     public PartCreativeEnergy(final ItemStack is) {
         super(is);
-        this.getProxy().setIdlePowerUsage(0);
-        this.getProxy().setFlags(GridFlags.CANNOT_CARRY);
-        this.outerProxy.setIdlePowerUsage(0);
-        this.outerProxy.setFlags(GridFlags.CANNOT_CARRY);
     }
 
     @Override
@@ -71,49 +51,8 @@ public class PartCreativeEnergy extends AEBasePart implements IAEPowerStorage {
     }
 
     @Override
-    public void readFromNBT(final NBTTagCompound extra) {
-        super.readFromNBT(extra);
-        this.outerProxy.readFromNBT(extra);
-    }
-
-    @Override
-    public void writeToNBT(final NBTTagCompound extra) {
-        super.writeToNBT(extra);
-        this.outerProxy.writeToNBT(extra);
-    }
-
-    @Override
-    public void removeFromWorld() {
-        super.removeFromWorld();
-        this.outerProxy.invalidate();
-    }
-
-    @Override
-    public void addToWorld() {
-        super.addToWorld();
-        this.outerProxy.onReady();
-    }
-
-    @Override
-    public void setPartHostInfo(final ForgeDirection side, final IPartHost host, final TileEntity tile) {
-        super.setPartHostInfo(side, host, tile);
-        this.outerProxy.setValidSides(EnumSet.of(side));
-    }
-
-    @Override
-    public IGridNode getExternalFacingNode() {
-        return this.outerProxy.getNode();
-    }
-
-    @Override
     public int cableConnectionRenderTo() {
         return 16;
-    }
-
-    @Override
-    public void onPlacement(final EntityPlayer player, final ItemStack held, final ForgeDirection side) {
-        super.onPlacement(player, held, side);
-        this.outerProxy.setOwner(player);
     }
 
     @Override
