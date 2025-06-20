d19a5c4f916a09a2346b5d3c7b06692d223ec0d3
Martin Robertz <dream-master@gmx.net>
2025-06-20 21:41:18 +0200

    update sa

--- src/main/java/appeng/client/gui/widgets/GuiImgButton.java
+++ src/main/java/appeng/client/gui/widgets/GuiImgButton.java
@@ -881,7 +881,7 @@ public class GuiImgButton extends GuiButton implements ITooltip {
                     ButtonToolTips.PinsSection,
                     ButtonToolTips.PinsSectionActive);
 
-  this.registerApp(
+            this.registerApp(
                     16 * 6 + 9,
                     Settings.SUPER_WIRELESS_TOOL_GROUP_BY,
                     SuperWirelessToolGroupBy.Single,
